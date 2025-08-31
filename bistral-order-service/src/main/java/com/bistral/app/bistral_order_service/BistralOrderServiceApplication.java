4ff25088cfcf8570e4e4bc53277e597f20f55101
Ruddarm <ruddarmmaurya@gmail.com>
2025-08-31 18:57:04 +0530

    init order service

---
+++ bistral-order-service/src/main/java/com/bistral/app/bistral_order_service/BistralOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bistral.app.bistral_order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BistralOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BistralOrderServiceApplication.class, args);
+	}
+
+}
---
+++ bistral-order-service/src/test/java/com/bistral/app/bistral_order_service/BistralOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bistral.app.bistral_order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BistralOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
